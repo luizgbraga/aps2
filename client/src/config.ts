ae969e38d953dd7e0cc1e003d24ba684b596d0a6
luizgbraga <luizb.07@outlook.com>
2024-05-23 09:37:19 -0300

    adding types adn refactor

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,3 +2,5 @@ export const API_URL =
   import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
 export const CLIENT_URL =
   import.meta.env.VITE_CLIENT_URL || 'http://localhost:3000';
+export const MAPS_API_KEY =
+  import.meta.env.VITE_REACT_APP_MAPS_API_KEY || 'api-key';
--- client/src/routes/user/Home.tsx
+++ client/src/routes/user/Home.tsx
@@ -3,18 +3,29 @@ import React from 'react';
 import { LoggedLayout } from '../../layout/logged/LoggedLayout';
 import { Status, Wrapper } from '@googlemaps/react-wrapper';
 import MapComponent from '../../components/MapComponent';
-
+import { MAPS_API_KEY } from '../../config';
+import { Flex, Spin } from 'antd';
 
 export const Home: React.FC = () => {
-  const apiKey = import.meta.env.VITE_REACT_APP_MAPS_API_KEY ?? 'API_KEY_NOT_SET';
-  const render = (status: Status) => (<h1>{status}</h1>)
+  const render = (status: Status) => {
+    if (status === Status.LOADING) {
+      return (
+        <Flex
+          justify="center"
+          align="center"
+          style={{ width: '100%', height: '100%' }}
+        >
+          <Spin size="large" />
+        </Flex>
+      );
+    }
+    return <span />;
+  };
   return (
     <LoggedLayout selected="home">
-      <div className="App">
-        <Wrapper apiKey={apiKey} render={render}>
-          <MapComponent />
-        </Wrapper>
-      </div>
+      <Wrapper apiKey={MAPS_API_KEY} render={render}>
+        <MapComponent />
+      </Wrapper>
     </LoggedLayout>
   );
 };
