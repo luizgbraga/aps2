3c8880076f52242c683e39f3d2e1b77aa158ad85
IvoLinux <101259836+IvoLinux@users.noreply.github.com>
2024-05-24 12:20:40 -0300

    drawPath with mock coordinates
    Missing logic for what path to draw. Currently draws whatever path in coordinates var.

--- client/src/components/useMap.tsx
+++ client/src/components/useMap.tsx
@@ -2,8 +2,16 @@ import { RefObject, useEffect, useState } from 'react';
 import { mapController } from './mapController';
 
 export const useMap = (ref: RefObject<HTMLDivElement | null>) => {
+  const coordinates = [
+    { lat: -22.954910561702736, lng: -43.165966423618656 },
+    { lat: -22.953051697730086, lng: -43.17171294459636 },
+    { lat: -22.951450792163904, lng: -43.17499447371 },
+    { lat: -22.95435355374614, lng: -43.17723481083563 },
+    { lat: -22.957009719130134, lng: -43.17692137895785 },
+  ];
+
   const [map, setMap] = useState<google.maps.Map | null>(null);
-  const { setup, setLocationToCurrent, addRecentralizeButton } =
+  const { setup, setLocationToCurrent, addRecentralizeButton, drawPath } =
     mapController();
   useEffect(() => {
     if (ref.current && !map) {
@@ -12,6 +20,7 @@ export const useMap = (ref: RefObject<HTMLDivElement | null>) => {
     if (map) {
       setLocationToCurrent(map);
       addRecentralizeButton(map);
+      mapController().drawPath(map, coordinates);
     }
   }, [map]);
 
