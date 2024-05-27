f1b1ec0a84165a1a440b3046ae886937d55eaa8d
luizgbraga <luizb.07@outlook.com>
2024-05-27 08:58:28 -0300

    subscription routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,11 +2,13 @@ import { Application } from 'express';
 import userRoutes from './entities/user/routes';
 import adminRoutes from './entities/admin/routes';
 import occurrenceRoutes from './entities/occurrence/routes';
+import subscriptionRoutes from './entities/subscription/routes';
 
 export class Routes {
   constructor(app: Application) {
     app.use('/api/user', userRoutes);
     app.use('/api/admin', adminRoutes);
     app.use('/api/occurrence', occurrenceRoutes);
+    app.use('/api/subscription', subscriptionRoutes);
   }
 }
