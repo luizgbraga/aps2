14f8cc62be0370b0a352314e2cbc7350b7e99424
luizgbraga <luizb.07@outlook.com>
2024-05-27 10:06:01 -0300

    subscribe todo

--- client/src/routes/ocurrences/Occurences.tsx
+++ client/src/routes/ocurrences/Occurences.tsx
@@ -1,24 +1,36 @@
-import { Space } from 'antd';
-import React, { useEffect, useState } from 'react';
+import { Button, Drawer, Select, Space } from 'antd';
+import React, { useState } from 'react';
 import { OccurenceModel } from '../../api/occurences';
 import { BaseLayout } from '../../layout/BaseLayout';
 import Cards from './Card';
+import { useAsync } from '../../utils/async';
+import { NeighborhoodModel } from '../../api/neighborhood';
 
 const Notifications: React.FC = () => {
-  const [occurenceList, setOccurenceList] = useState<OccurenceModel[]>([]);
+  const { result: occurenceList } = useAsync(() => OccurenceModel.list());
+  const { result: neighborhoods } = useAsync(() => NeighborhoodModel.list());
+  const [openDrawer, setOpenDrawer] = useState(false);
 
-  useEffect(() => {
-    const fetchOccurences = async () => {
-      const list = await OccurenceModel.list();
-      setOccurenceList(list);
-    };
-    fetchOccurences();
-  }, []);
+  const subscribe = () => {
+    console.log('todo');
+  };
 
   return (
-    <BaseLayout selected="notifications" title="Notifications">
+    <BaseLayout
+      selected="notifications"
+      title="Notifications"
+      extra={
+        <Button
+          type="primary"
+          style={{ marginTop: '10px' }}
+          onClick={() => setOpenDrawer(true)}
+        >
+          Subscribe
+        </Button>
+      }
+    >
       <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-        {occurenceList.map((occ) => (
+        {occurenceList?.map((occ) => (
           <Cards
             key={occ.id}
             {...{
@@ -32,6 +44,23 @@ const Notifications: React.FC = () => {
           />
         ))}
       </Space>
+      <Drawer
+        title="Subscribe to neighborhood"
+        placement="right"
+        closable={true}
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+        width={500}
+      >
+        <Select
+          placeholder="Select neighborhood"
+          style={{ width: '100%' }}
+          options={neighborhoods?.map((n) => ({ label: n.name, value: n.id }))}
+        />
+        <Button type="primary" onClick={subscribe}>
+          Bora
+        </Button>
+      </Drawer>
     </BaseLayout>
   );
 };
