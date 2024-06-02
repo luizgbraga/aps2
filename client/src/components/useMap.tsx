2c9f1a944c950e455eac984105eb4f01b74c6a62
IvoLinux <101259836+IvoLinux@users.noreply.github.com>
2024-06-02 17:50:16 -0300

    super gay bowser
    Removing items from the filter actually removes their drawings on the map.

--- client/src/components/useMap.tsx
+++ client/src/components/useMap.tsx
@@ -21,16 +21,27 @@ async function fetchTrips(targetRouteId: string[]) {
       color = '#' + route.color;
       if (route.text_color) text_color = route.text_color;
     }
-    return { trip: trip, color: color, text_color: text_color };
+    return {
+      route_id: route?.id,
+      trip: trip,
+      color: color,
+      text_color: text_color,
+    };
   });
 }
 
 async function fetchTripsShapes(
-  trips: { trip: TripDTO; color: string; text_color: string }[]
+  trips: {
+    route_id: string | undefined;
+    trip: TripDTO;
+    color: string;
+    text_color: string;
+  }[]
 ) {
   const shapePromises = trips.map(async (element) => {
     const shape = await ShapesModel.getShape(element.trip.id);
     return {
+      route_id: element.route_id,
       shape: shape,
       color: element.color,
       text_color: element.text_color,
@@ -39,6 +50,7 @@ async function fetchTripsShapes(
   const shapesObject = await Promise.all(shapePromises);
   const mappedShapes = shapesObject.map((shapeObject) => {
     return {
+      route_id: shapeObject.route_id,
       shape: shapeObject.shape.map((shape) => {
         return { lat: shape.pt_lat, lng: shape.pt_lon };
       }),
@@ -53,28 +65,30 @@ export const useMap = (
   ref: RefObject<HTMLDivElement | null>,
   pinnable: boolean
 ) => {
-  const [map, setMap] = useState<google.maps.Map | null>(null);
   const {
     setup,
     setLocationToCurrent,
     addRecentralizeButton,
     drawPaths,
     addMarker,
+    clearPaths,
   } = mapController();
+  const [map, setMap] = useState<google.maps.Map | null>(null);
   const [paths, setPaths] = useState<
     {
+      route_id: string | undefined;
       shape: { lat: number; lng: number }[];
       color: string;
       text_color: string;
     }[]
   >([]);
   const [routeIds, setRouteIds] = useState<string[]>([]);
+  const [routeIdsToDraw, setRouteIdsToDraw] = useState<string[]>([]);
+  const [polylines, setPolylines] = useState<
+    { route_id: string; polyline: google.maps.Polyline }[]
+  >([]);
   const prevMarkersRef = useRef([] as google.maps.Marker[]);
   const { result: allOccurences } = useAsync(() => OccurenceModel.all());
-
-  const changeRouteIds = (newRouteIds: string[]) => {
-    setRouteIds(newRouteIds);
-  };
 
   map?.addListener('click', (e: any) => {
     if (!pinnable) return;
@@ -109,35 +123,42 @@ export const useMap = (
         });
       }
     }
-  }, [
-    addRecentralizeButton,
-    map,
-    ref,
-    setLocationToCurrent,
-    setup,
-    pinnable,
-    allOccurences,
-    addMarker,
-  ]);
-
-  useEffect(() => {
-    const fetchTripsAndShapes = async () => {
-      const trips = await fetchTrips(routeIds);
-      const paths = await fetchTripsShapes(trips);
-      setPaths(paths);
-    };
-    fetchTripsAndShapes();
-  }, [map, routeIds]);
-
-  useEffect(() => {
-    if (map && paths) drawPaths(map, paths);
-  }, [drawPaths, map, paths]);
+  }, [map, ref, pinnable, allOccurences]);
 
   function clearMarkers(markers: google.maps.Marker[]) {
     for (const m of markers) {
       m.setMap(null);
     }
   }
+
+  // changeRouteIds is called when the user updates the filter. It (and the following useEffects) draws the newly selected lines and removes any that were removed
+  const changeRouteIds = (newRouteIds: string[]) => {
+    const addedRoutes = newRouteIds.filter(
+      (route) => !routeIds.includes(route)
+    );
+    const removedRoutes = routeIds.filter(
+      (route) => !newRouteIds.includes(route)
+    );
+    setRouteIds(newRouteIds);
+    setRouteIdsToDraw(addedRoutes);
+    const polylinesToRemove = polylines.filter((polyline) =>
+      removedRoutes.includes(polyline.route_id)
+    );
+    clearPaths(polylinesToRemove);
+  };
+
+  useEffect(() => {
+    const fetchTripsAndShapes = async () => {
+      const trips = await fetchTrips(routeIdsToDraw);
+      const paths = await fetchTripsShapes(trips);
+      setPaths(paths);
+    };
+    fetchTripsAndShapes();
+  }, [routeIdsToDraw]);
+
+  useEffect(() => {
+    if (map && paths) drawPaths(map, paths, setPolylines);
+  }, [paths]);
 
   return {
     map,
